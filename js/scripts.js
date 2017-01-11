a1f328fcfc15438093b8c7db20fa4fc062ef2561
Derek Villars and Tammy Dang <derek@>
2017-01-10 16:08:15 -0800

    add responsive dialogue between user and walrus

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,54 @@ $(document).ready(function() {
     $("#walrus-showing").slideToggle();
     $("#walrus-hidden").slideToggle();
   });
-});
-$(document).ready(function() {
   $(".fact-click").click(function() {
     $('#fade-in').fadeToggle();
+  });
+  $('#green').click(function() {
+    $("body").removeClass();
+    $('body').addClass('green-bg');
+  });
+  $('#yellow').click(function() {
+    $("body").removeClass();
+    $('body').addClass('yellow-bg');
+  });
+  $('#red').click(function() {
+    $("body").removeClass();
+    $('body').addClass('red-bg');
+  });
+
+
+
+  $("#hello").click(function() {
+    $("#user").prepend("<li>Hello!</li>");
+    $("#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("#goodbye").click(function() {
+    $("#user").prepend("<li>Goodbye!</li>");
+    $("#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("#stop").click(function() {
+    $("#user").prepend("<li>Stop copying me!</li>");
+    $("#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 });
